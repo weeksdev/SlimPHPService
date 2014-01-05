254796943e91c85a46422c9b5a6dbdcb7b2b13b1
Andrew Weeks <adweeks@gmail.com>
2014-01-04 19:55:44 -0600

    A Simple Slim PHP Web Service Example
    A Simple Slim PHP Web Service Example

---
+++ test1.php
@@ -0,0 +1,14 @@
+﻿<?php
+	require 'Slim/Slim/Slim.php';
+	\Slim\Slim::registerAutoloader();
+	$app = new \Slim\Slim();
+
+	$app->get('/hello/:name', function ($name) {
+    	echo "Hello, $name";
+	});
+	
+	$app->get('/phone/:firstname/:lastname', function($firstname, $lastname){
+		echo 'Oh No,'.$firstname.$lastname;
+	});
+	$app->run();
+?>
